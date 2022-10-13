52461587473daa2977fae3a5813d9524b81478c2
LucasCruz <lca.snf21@uea.edu.br>
2022-10-13 16:47:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# casa_dos_jogos
+ 
---
+++ casa_dos_jogos/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for casa_dos_jogos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'casa_dos_jogos.settings')
+
+application = get_wsgi_application()
