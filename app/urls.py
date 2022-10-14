7331d475f4b4fe708a64e2a7622bde85cab39c68
LucasCruz <lca.snf21@uea.edu.br>
2022-10-14 16:06:55 -0400

    Template cadastro adicionado

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
+    path('cadastro/', views.cadastro),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import redirect, render
 
 def home(request):
     return render(request, 'home.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
