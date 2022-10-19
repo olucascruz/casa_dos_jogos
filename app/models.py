3f7febaf5609af34fb801e916f0b73f211e5680c
LucasCruz <lca.snf21@uea.edu.br>
2022-10-19 01:05:28 -0400

    create model user

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User():
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField()
