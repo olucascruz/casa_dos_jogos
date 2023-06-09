7811c5cfdbf12e1c287a3053a656d898c200a943
LucasCruz <lca.snf21@uea.edu.br>
2023-06-09 12:33:20 -0400

    dev: add config auto cache delete

--- casa_dos_jogos/settings.py
+++ casa_dos_jogos/settings.py
@@ -135,3 +135,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'app/static/img')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
